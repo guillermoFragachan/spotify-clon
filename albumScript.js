b7893c3531e2af1cb483ba6e3e8e13d420c4bee5
JohnBl0x <johnbloxam2@gmail.com>
2021-09-08 11:58:53 +0100

    Has this worked?

--- albumScript.js
+++ albumScript.js
@@ -5,7 +5,8 @@ function changeTrack1() {
     let audio = document.getElementById("audio");
     audio.play();
     document.getElementById("track1").style.backgroundColor="darkSlateGray";
-    document.getElementById("playing").innerHTML = "NOW PLAYING: <strong>Queen - 20th Century Fox Fanfare</strong>";
+    document.getElementById("playingText").innerHTML = "NOW PLAYING: <strong>Queen - 20th Century Fox Fanfare</strong>";
+    document.getElementById("stopIcon").style.fill="#1CBB52";
 }
 
 
@@ -13,57 +14,67 @@ function changeTrack2() {
     let audio = document.getElementById("audio2");
     audio.play();
     document.getElementById("track2").style.backgroundColor="darkSlateGray";
-    document.getElementById("playing").innerHTML = "NOW PLAYING: <strong>Queen - Somebody To Love</strong>";
+    document.getElementById("playingText").innerHTML = "NOW PLAYING: <strong>Queen - Somebody To Love</strong>";
+    document.getElementById("stopIcon").style.fill="#1CBB52";
 }
 
 function changeTrack3() {
     let audio = document.getElementById("audio3");
     audio.play();
     document.getElementById("track3").style.backgroundColor="darkSlateGray";
-    document.getElementById("playing").innerHTML = "NOW PLAYING: <strong>Queen - Doing All Right - ...Revisited</strong>";
+    document.getElementById("playingText").innerHTML = "NOW PLAYING: <strong>Queen - Doing All Right - ...Revisited</strong>";
+    document.getElementById("stopIcon").style.fill="#1CBB52";
 }
 
 function changeTrack4() {
     let audio = document.getElementById("audio4");
     audio.play();
     document.getElementById("track4").style.backgroundColor="darkSlateGray";
-    document.getElementById("playing").innerHTML = "NOW PLAYING: <strong>Queen - Keep Yourself Alive - Live At The Rainbow</strong>";
+    document.getElementById("playingText").innerHTML = "NOW PLAYING: <strong>Queen - Keep Yourself Alive - Live At The Rainbow</strong>";
+    document.getElementById("stopIcon").style.fill="#1CBB52";
 }
 
 function changeTrack5() {
     let audio = document.getElementById("audio5");
     audio.play();
     document.getElementById("track5").style.backgroundColor="darkSlateGray";
-    document.getElementById("playing").innerHTML = "NOW PLAYING: <strong>Queen - Killer Queen</strong>";
+    document.getElementById("playingText").innerHTML = "NOW PLAYING: <strong>Queen - Killer Queen</strong>";
+    document.getElementById("stopIcon").style.fill="#1CBB52";
 }
 
 function changeTrack6() {
     let audio = document.getElementById("audio6");
     audio.play();
     document.getElementById("track6").style.backgroundColor="darkSlateGray";
-    document.getElementById("playing").innerHTML = "NOW PLAYING: <strong>Queen - Fat Bottomed Girls</strong>";
+    document.getElementById("playingText").innerHTML = "NOW PLAYING: <strong>Queen - Fat Bottomed Girls</strong>";
+    document.getElementById("stopIcon").style.fill="#1CBB52";
 }
 
 function changeTrack7() {
     let audio = document.getElementById("audio7");
     audio.play();
     document.getElementById("track7").style.backgroundColor="darkSlateGray";
-    document.getElementById("playing").innerHTML = "NOW PLAYING: <strong>Queen - Bohemian Rhapsody</strong>";
+    document.getElementById("playingText").innerHTML = "NOW PLAYING: <strong>Queen - Bohemian Rhapsody</strong>";
+    document.getElementById("stopIcon").style.fill="#1CBB52";
 }
 
 function changeTrack8() {
     let audio = document.getElementById("audio8");
     audio.play();
     document.getElementById("track8").style.backgroundColor="darkSlateGray";
-    document.getElementById("playing").innerHTML = "NOW PLAYING: <strong>Queen - Now I'm Here - Live At The Hammersmith Odeon</strong>";
+    document.getElementById("playingText").innerHTML = "NOW PLAYING: <strong>Queen - Now I'm Here - Live At The Hammersmith Odeon</strong>";
+    document.getElementById("stopIcon").style.fill="#1CBB52";
 }
 
 function changeTrack9() {
     let audio = document.getElementById("audio9");
     audio.play();
     document.getElementById("track9").style.backgroundColor="darkSlateGray";
-    document.getElementById("playing").innerHTML = "NOW PLAYING: <strong>Queen - Crazy Little Thing Called Love</strong>";
+    document.getElementById("playingText").innerHTML = "NOW PLAYING: <strong>Queen - Crazy Little Thing Called Love</strong>";
+    document.getElementById("stopIcon").style.fill="#1CBB52";
 }
+
+
 
 
 

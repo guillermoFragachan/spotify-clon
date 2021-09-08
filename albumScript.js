4cf3924e58df560e79e364cffc70159d475d1d58
JohnBl0x <johnbloxam2@gmail.com>
2021-09-08 13:21:03 +0100

    Is this working?

--- albumScript.js
+++ albumScript.js
@@ -5,7 +5,7 @@ function changeTrack1() {
     let audio = document.getElementById("audio");
     audio.play();
     document.getElementById("track1").style.backgroundColor="darkSlateGray";
-    document.getElementById("playingText").innerHTML = "NOW PLAYING: <strong>Queen - 20th Century Fox Fanfare</strong>";
+    document.getElementById("playingText").innerHTML = "NOW PLAYING: <strong>20th Century Fox Fanfare (Queen)</strong>";
     document.getElementById("stopIcon").style.fill="#1CBB52";
 }
 
@@ -14,7 +14,7 @@ function changeTrack2() {
     let audio = document.getElementById("audio2");
     audio.play();
     document.getElementById("track2").style.backgroundColor="darkSlateGray";
-    document.getElementById("playingText").innerHTML = "NOW PLAYING: <strong>Queen - Somebody To Love</strong>";
+    document.getElementById("playingText").innerHTML = "NOW PLAYING: <strong>Somebody To Love (Queen)</strong>";
     document.getElementById("stopIcon").style.fill="#1CBB52";
 }
 
@@ -22,7 +22,7 @@ function changeTrack3() {
     let audio = document.getElementById("audio3");
     audio.play();
     document.getElementById("track3").style.backgroundColor="darkSlateGray";
-    document.getElementById("playingText").innerHTML = "NOW PLAYING: <strong>Queen - Doing All Right - ...Revisited</strong>";
+    document.getElementById("playingText").innerHTML = "NOW PLAYING: <strong>Doing All Right - ...Revisited (Smile)</strong>";
     document.getElementById("stopIcon").style.fill="#1CBB52";
 }
 
@@ -30,7 +30,7 @@ function changeTrack4() {
     let audio = document.getElementById("audio4");
     audio.play();
     document.getElementById("track4").style.backgroundColor="darkSlateGray";
-    document.getElementById("playingText").innerHTML = "NOW PLAYING: <strong>Queen - Keep Yourself Alive - Live At The Rainbow</strong>";
+    document.getElementById("playingText").innerHTML = "NOW PLAYING: <strong>Keep Yourself Alive - Live At The Rainbow (Queen)</strong>";
     document.getElementById("stopIcon").style.fill="#1CBB52";
 }
 
@@ -38,7 +38,7 @@ function changeTrack5() {
     let audio = document.getElementById("audio5");
     audio.play();
     document.getElementById("track5").style.backgroundColor="darkSlateGray";
-    document.getElementById("playingText").innerHTML = "NOW PLAYING: <strong>Queen - Killer Queen</strong>";
+    document.getElementById("playingText").innerHTML = "NOW PLAYING: <strong>Killer Queen (Queen)</strong>";
     document.getElementById("stopIcon").style.fill="#1CBB52";
 }
 
@@ -46,7 +46,7 @@ function changeTrack6() {
     let audio = document.getElementById("audio6");
     audio.play();
     document.getElementById("track6").style.backgroundColor="darkSlateGray";
-    document.getElementById("playingText").innerHTML = "NOW PLAYING: <strong>Queen - Fat Bottomed Girls</strong>";
+    document.getElementById("playingText").innerHTML = "NOW PLAYING: <strong>Fat Bottomed Girls (Queen)</strong>";
     document.getElementById("stopIcon").style.fill="#1CBB52";
 }
 
@@ -54,7 +54,7 @@ function changeTrack7() {
     let audio = document.getElementById("audio7");
     audio.play();
     document.getElementById("track7").style.backgroundColor="darkSlateGray";
-    document.getElementById("playingText").innerHTML = "NOW PLAYING: <strong>Queen - Bohemian Rhapsody</strong>";
+    document.getElementById("playingText").innerHTML = "NOW PLAYING: <strong>Bohemian Rhapsody (Queen)</strong>";
     document.getElementById("stopIcon").style.fill="#1CBB52";
 }
 
@@ -62,7 +62,7 @@ function changeTrack8() {
     let audio = document.getElementById("audio8");
     audio.play();
     document.getElementById("track8").style.backgroundColor="darkSlateGray";
-    document.getElementById("playingText").innerHTML = "NOW PLAYING: <strong>Queen - Now I'm Here - Live At The Hammersmith Odeon</strong>";
+    document.getElementById("playingText").innerHTML = "NOW PLAYING: <strong>Now I'm Here - Live At The Hammersmith Odeon (Queen)</strong>";
     document.getElementById("stopIcon").style.fill="#1CBB52";
 }
 
@@ -70,7 +70,7 @@ function changeTrack9() {
     let audio = document.getElementById("audio9");
     audio.play();
     document.getElementById("track9").style.backgroundColor="darkSlateGray";
-    document.getElementById("playingText").innerHTML = "NOW PLAYING: <strong>Queen - Crazy Little Thing Called Love</strong>";
+    document.getElementById("playingText").innerHTML = "NOW PLAYING: <strong>Crazy Little Thing Called Love (Queen)</strong>";
     document.getElementById("stopIcon").style.fill="#1CBB52";
 }
 

614fbdbaf2bce0cdd7a2b5ebb2ac3222ca8e5136
JohnBl0x <johnbloxam2@gmail.com>
2021-09-08 11:20:20 +0100

    Songs Play When Clicked

---
+++ albumScript.js
@@ -0,0 +1,70 @@
+
+
+
+function changeTrack1() {
+    let audio = document.getElementById("audio");
+    audio.play();
+    document.getElementById("track1").style.backgroundColor="darkSlateGray";
+    document.getElementById("playing").innerHTML = "NOW PLAYING: <strong>Queen - 20th Century Fox Fanfare</strong>";
+}
+
+
+function changeTrack2() {
+    let audio = document.getElementById("audio2");
+    audio.play();
+    document.getElementById("track2").style.backgroundColor="darkSlateGray";
+    document.getElementById("playing").innerHTML = "NOW PLAYING: <strong>Queen - Somebody To Love</strong>";
+}
+
+function changeTrack3() {
+    let audio = document.getElementById("audio3");
+    audio.play();
+    document.getElementById("track3").style.backgroundColor="darkSlateGray";
+    document.getElementById("playing").innerHTML = "NOW PLAYING: <strong>Queen - Doing All Right - ...Revisited</strong>";
+}
+
+function changeTrack4() {
+    let audio = document.getElementById("audio4");
+    audio.play();
+    document.getElementById("track4").style.backgroundColor="darkSlateGray";
+    document.getElementById("playing").innerHTML = "NOW PLAYING: <strong>Queen - Keep Yourself Alive - Live At The Rainbow</strong>";
+}
+
+function changeTrack5() {
+    let audio = document.getElementById("audio5");
+    audio.play();
+    document.getElementById("track5").style.backgroundColor="darkSlateGray";
+    document.getElementById("playing").innerHTML = "NOW PLAYING: <strong>Queen - Killer Queen</strong>";
+}
+
+function changeTrack6() {
+    let audio = document.getElementById("audio6");
+    audio.play();
+    document.getElementById("track6").style.backgroundColor="darkSlateGray";
+    document.getElementById("playing").innerHTML = "NOW PLAYING: <strong>Queen - Fat Bottomed Girls</strong>";
+}
+
+function changeTrack7() {
+    let audio = document.getElementById("audio7");
+    audio.play();
+    document.getElementById("track7").style.backgroundColor="darkSlateGray";
+    document.getElementById("playing").innerHTML = "NOW PLAYING: <strong>Queen - Bohemian Rhapsody</strong>";
+}
+
+function changeTrack8() {
+    let audio = document.getElementById("audio8");
+    audio.play();
+    document.getElementById("track8").style.backgroundColor="darkSlateGray";
+    document.getElementById("playing").innerHTML = "NOW PLAYING: <strong>Queen - Now I'm Here - Live At The Hammersmith Odeon</strong>";
+}
+
+function changeTrack9() {
+    let audio = document.getElementById("audio9");
+    audio.play();
+    document.getElementById("track9").style.backgroundColor="darkSlateGray";
+    document.getElementById("playing").innerHTML = "NOW PLAYING: <strong>Queen - Crazy Little Thing Called Love</strong>";
+}
+
+
+
+

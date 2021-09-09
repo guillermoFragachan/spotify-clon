502f3f4764e180474a6faa07d129098266deb84a
Matīss Komļevs <matisskomlevs@gmail.com>
2021-09-09 17:40:10 +0300

    Can add playlist name

--- addPlaylist.js
+++ addPlaylist.js
@@ -2,6 +2,9 @@ const open =  document.getElementById("open");
 const modal_container = document.getElementById("modal_container");
 const close = document.getElementById("close");
 
+const playlistName = document.getElementById('playlistName')
+const playlisyList = document.getElementById("playlist_list")
+
 const openModal = function () {
   open.addEventListener("click", () => {
     modal_container.classList.add("show")
@@ -10,4 +13,19 @@ const openModal = function () {
   close.addEventListener("click", () => {
     modal_container.classList.remove("show")
   })
+}
+
+
+const addPlaylist = function() {
+  liNode = document.createElement("li")
+  liNode.classList.add("nav-item")
+  liNode.appendChild(document.createTextNode(playlistName.value))
+  playlisyList.appendChild(liNode)
+
+  const innerLinkNode = document.createElement("a")
+  innerLinkNode.classList.add("nav-link")
+  innerLinkNode.setAttribute("href", "#")
+  liNode.appendChild(innerLinkNode)
+  
+  innerLinkNode
 }
